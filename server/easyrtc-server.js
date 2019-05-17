a78462c6b0e4ccfe02bc2c66b05fda72a091cada
Andrei Homentcovschi <ahomentc@gmail.com>
2019-05-17 15:26:51 -0700

    Fixed easyrtc instead of wseasyrtc. Compartamentalized scene folders. Fixed theater

--- server/easyrtc-server.js
+++ server/easyrtc-server.js
@@ -30,11 +30,11 @@ var funct = require('./functions.js'); //funct file contains our helper function
 process.title = "node-easyrtc";
 
 // Get port or default to 8080
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8090;
 
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
 const host = '127.0.0.1'
-// host = '192.168.0.104'
+// host = '192.168.0.102'
 var app = express(host);
 
 
